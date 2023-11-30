f3bb7baa0162250ebf75551d291cd15b5278048a
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-11-30 11:18:25 +0100

    Add @vercel/analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
