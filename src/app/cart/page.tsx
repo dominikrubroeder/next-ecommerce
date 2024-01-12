bd6b5f6d9ae74906854dfa800b4db154a41abdbd
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-01-12 08:45:24 +0100

    Link from Cart Page to Checkout Page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function CartPage() {
   return (
     <div className="p-4">
       <h1 className="text-2xl">Hello, world – Cart</h1>
+      <Link href="/checkout">Checkout</Link>
     </div>
   );
 }
