88bcf84c77569710430e19ac6d19cb8115e90788
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-01-12 14:13:59 +0100

    Create reusable BackLink Component

---
+++ src/components/BackLink.tsx
@@ -0,0 +1,22 @@
+import { ArrowLongLeftIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function BackLink({ href }: { href: string }) {
+  const url = href.replace("/", "");
+
+  return (
+    <Link
+      href={href}
+      className="my-4 flex items-center gap-2"
+      title={`Go back to ${url} page`}
+      aria-label={`Go back to ${url} page`}
+    >
+      <ArrowLongLeftIcon
+        className="h-5 w-5"
+        title="Arrow long left icon"
+        aria-label="Arrow long left icon"
+      />
+      Back
+    </Link>
+  );
+}
