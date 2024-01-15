4069ec567aa6467f5c8d9d806f5ec903a921a9a5
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-01-15 11:53:48 +0100

    Delete unused CategoryProducts.tsx and CategoryProductsLoading.tsx Component

--- src/components/categories/products/CategoryProductsLoading.tsx
+++
@@ -1,7 +0,0 @@
-export default function CategoryProductsLoading() {
-  return (
-    <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
-      <li className="h-8 w-full rounded-full bg-gray-100"></li>
-    </ul>
-  );
-}
