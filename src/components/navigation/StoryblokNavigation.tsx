1d2e75cccb841657a73fd90f187746780e2ab333
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-01-16 09:43:29 +0100

    unstable_noStore() next/cache method in StoryblokNavigation.tsx to always fetch latest Storyblok Navigation state

--- src/components/navigation/StoryblokNavigation.tsx
+++ src/components/navigation/StoryblokNavigation.tsx
@@ -1,8 +1,11 @@
 import { fetchStoryblokStory } from "@/lib/storyblok";
 import { StoryblokComponent } from "@storyblok/react";
 import React from "react";
+import { unstable_noStore } from "next/cache";
 
 export default async function StoryblokNavigation() {
+  unstable_noStore();
+
   const { data: config } = await fetchStoryblokStory({
     name: "/layout-config",
   });
