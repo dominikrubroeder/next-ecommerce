de04409fb9751e805d5ed0bc1295685be7d5b1a2
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-01-18 10:19:18 +0100

    Move StoryblokNavigation.tsx to components/navigation/storyblok-navigation

--- src/components/navigation/StoryblokNavigationLoading.tsx
+++
@@ -1,9 +0,0 @@
-export default function StoryblokNavigationLoading() {
-  return (
-    <ul className="flex animate-pulse gap-4">
-      <li className="w-32 rounded-full bg-gray-100"></li>
-      <li className="w-32 rounded-full bg-gray-100"></li>
-      <li className="w-32 rounded-full bg-gray-100"></li>
-    </ul>
-  );
-}
---
+++ src/components/navigation/storyblok-navigation/StoryblokNavigationLoading.tsx
@@ -0,0 +1,9 @@
+export default function StoryblokNavigationLoading() {
+  return (
+    <ul className="flex animate-pulse flex-wrap gap-2">
+      <li className="h-6 w-24 rounded-full border bg-gray-100"></li>
+      <li className="h-6 w-24 rounded-full border bg-gray-100"></li>
+      <li className="h-6 w-24 rounded-full border bg-gray-100"></li>
+    </ul>
+  );
+}
