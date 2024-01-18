d676ab0eaadb4c3c64f23d2cff7a9d45c7f5acc0
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-01-18 10:23:44 +0100

    Optimize Header.tsx with respective Sub-Components for mobile use

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,38 +1,42 @@
 import Link from "next/link";
 import Search from "@/components/search/Search";
 import { Suspense } from "react";
-import StoryblokNavigation from "@/components/navigation/StoryblokNavigation";
-import StoryblokNavigationLoading from "@/components/navigation/StoryblokNavigationLoading";
+import StoryblokNavigation from "@/components/navigation/storyblok-navigation/StoryblokNavigation";
+import StoryblokNavigationLoading from "@/components/navigation/storyblok-navigation/StoryblokNavigationLoading";
 import Logo from "@/components/Logo";
 
 export default function Header() {
   return (
-    <header className="grid gap-2 p-4 sm:flex sm:flex-wrap sm:items-center sm:justify-between sm:gap-4 sm:p-0 sm:pr-4">
+    <header className="mb-5 xl:flex xl:items-center xl:justify-between">
       <div className="grid gap-2 p-4 sm:flex sm:flex-wrap sm:items-center sm:justify-between sm:gap-4">
         <Logo />
 
         <nav>
-          <ul className="flex flex-wrap gap-2">
-            <li>
+          <ul className="flex items-center gap-2">
+            <li className="inline-block">
               <Link href="/categories">Categories</Link>
             </li>
-            <li>
+
+            <li className="inline-block">
               <Link href="/products">Products</Link>
             </li>
 
-            <Suspense fallback={<StoryblokNavigationLoading />}>
-              <StoryblokNavigation />
-            </Suspense>
+            <li>
+              <Suspense fallback={<StoryblokNavigationLoading />}>
+                <StoryblokNavigation />
+              </Suspense>
+            </li>
           </ul>
         </nav>
       </div>
 
-      <div className="flex items-center gap-4">
-        <Search placeholder="Products..." />
+      <div className="flex items-center justify-between gap-4 px-4">
+        <Search placeholder="Search anything..." />
 
-        <Link href="/account">Account</Link>
-
-        <Link href="/cart">Cart</Link>
+        <div className="space-x-4">
+          <Link href="/account">Account</Link>
+          <Link href="/cart">Cart</Link>
+        </div>
       </div>
     </header>
   );
