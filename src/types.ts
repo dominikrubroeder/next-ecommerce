e2fc0b19d4e24af910e2dbebefd44d7cd73cb6fe
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-01-25 12:21:28 +0100

    feature(Products, Sorting) – Read and write sorting option to the URL with useUpdateSearchParams

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,18 @@
-export type SortingOptionTitle = "Title descending" | "Title ascending";
-export type SortingOptionValue = "titleDescending" | "titleAscending";
-
 export type SortingOption = {
-  title: SortingOptionTitle;
-  value: SortingOptionValue;
+  title: "Title descending" | "Title ascending";
+  value: "titleDescending" | "titleAscending";
+  icon: string;
+};
+
+export type FilterOption = {
+  group: "Price" | "Color";
+  title:
+    | "Price under 50"
+    | "Price 50 - 100"
+    | "Price over 100"
+    | "Red"
+    | "Blue";
+  value: "price-under-50" | "price-50-100" | "price-over-100" | "red" | "blue";
 };
 
 export type Provider = "github";
